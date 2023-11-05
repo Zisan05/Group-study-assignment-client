10e4cb240084bb415f77ea4e8dcc74b2d513c830
Zisan05 <mdzisanislam69@gmail.com>
2023-11-06 02:11:50 +0800

    footer part done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
 <div className="hero-overlay bg-opacity-60"></div>
 <div className="hero-content text-center text-neutral-content">
   <div className="max-w-md">
-    <h1 className="mb-5 text-5xl font-bold text-amber-300 hover:text-fuchsia-400">When you share your knowledge it will increase!!</h1>
+    <h1 className="mb-5 text-5xl font-bold text-amber-300 hover:text-blue-300">When you share your knowledge it will increase!!</h1>
     <p className="mb-5 text-violet-400">Studying with group members is a great way to learn the syllabus more deeply & conceptually. Everyone has some unique talent and specific knowledge, in group study members can learn from each other. Students get an opportunity to take the advantages of other group members knowledge and talents.</p>
     <button className="btn bg-amber-300 font-bold">lets visit</button>
   </div>
